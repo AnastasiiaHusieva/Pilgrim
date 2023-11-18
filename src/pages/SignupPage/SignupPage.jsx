135754c236c3ad6df80d1394b7d60af44b1465aa
Anastasiia <anastasiaaaguseva@gmail.com>
2023-11-18 23:41:37 +0100

    to keep the user logged in after signing up+to show requirements for the password and email

--- src/pages/SignupPage/SignupPage.jsx
+++ src/pages/SignupPage/SignupPage.jsx
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import authService from '../../services/auth.service';
 import { useTheme } from '../../components//ThemeContext';
 import './SignupPage.css';
+import { AuthContext } from '../../context/auth.context'; 
 
 function SignupPage() {
   const { isDarkMode } = useTheme();
@@ -11,10 +12,25 @@ function SignupPage() {
   const [name, setName] = useState('');
   const [errorMessage, setErrorMessage] = useState(undefined);
   const [showPassword, setShowPassword] = useState(false);
+  const [showPasswordRequirements, setShowPasswordRequirements] = useState(false); // New state
   const navigate = useNavigate();
 
+  const { isLoggedIn, storeToken, authenticateUser } = useContext(AuthContext);
+
   const handleEmail = (e) => setEmail(e.target.value);
-  const handlePassword = (e) => setPassword(e.target.value);
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+    // Check password requirements and update the state accordingly
+    const hasUpperCase = /[A-Z]/.test(e.target.value);
+    const hasLowerCase = /[a-z]/.test(e.target.value);
+    const hasNumber = /\d/.test(e.target.value);
+    const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(e.target.value);
+    const isLengthValid = e.target.value.length >= 8;
+    setShowPasswordRequirements(
+      hasUpperCase && hasLowerCase && hasNumber && hasSpecialChar && isLengthValid
+    );
+  };
+
   const handleName = (e) => setName(e.target.value);
 
   const handleSignupSubmit = (e) => {
@@ -24,7 +40,9 @@ function SignupPage() {
     authService
       .signup(requestBody)
       .then((response) => {
-        navigate('/login');
+        storeToken(response.data.authToken);
+        authenticateUser();
+        navigate("/");
       })
       .catch((error) => {
         const errorDescription = error.response.data.message;
@@ -32,8 +50,13 @@ function SignupPage() {
       });
   };
 
-  return (
+  useEffect(() => {
+    if (isLoggedIn) {
+      navigate('/');
+    }
+  }, [isLoggedIn, navigate]);
 
+  return (
     <div className={`SignupPage min-h-screen flex items-center justify-center`}>
       <div className={`p-8 ${isDarkMode ? 'dark' : 'light'} rounded shadow-md w-70vw sm:w-96`}>
         <h1 className="text-2xl font-bold mb-6">Sign Up</h1>
@@ -75,6 +98,12 @@ function SignupPage() {
               </button>
             </div>
             <label className="label">Password:</label>
+            {showPasswordRequirements && (
+              <div className="text-sm text-gray-500 mt-2">
+                Password must have at least 8 characters, including uppercase, lowercase,
+                number, and special character.
+              </div>
+            )}
           </div>
 
           <button
@@ -88,7 +117,10 @@ function SignupPage() {
         {errorMessage && <p className="text-red-500 text-sm mt-4">{errorMessage}</p>}
 
         <p className="mt-4 text-gray-600">
-          Already have an account? <Link to="/login" className="text-pink-600 hover:text-pink-400">Login</Link>
+          Already have an account?{' '}
+          <Link to="/login" className="text-pink-600 hover:text-pink-400">
+            Login
+          </Link>
         </p>
       </div>
     </div>
