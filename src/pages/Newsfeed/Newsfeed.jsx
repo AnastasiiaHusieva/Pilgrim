f1aae6508d810fa0644eab26e2ed8b1286c86bf2
LeoD3v <leoargusa1991@gmail.com>
2023-11-29 22:52:21 +0100

    styles on newsfeed for scrolling

--- src/pages/Newsfeed/Newsfeed.jsx
+++ src/pages/Newsfeed/Newsfeed.jsx
@@ -1,101 +1,119 @@
-import { useEffect, useState, useContext } from 'react';
-import { Link } from 'react-router-dom';
-import axios from 'axios';
-import { AuthContext } from '../../context/auth.context';
-import { useParams } from 'react-router-dom';
+import { useEffect, useState, useContext } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import { AuthContext } from "../../context/auth.context";
+import { useParams } from "react-router-dom";
 
 function Newsfeed() {
-    const [posts, setPosts] = useState([]);
-    const authToken = localStorage.getItem("authToken");
-    const headers = {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${authToken}`,
-    };
-    const { user } = useContext(AuthContext);
-    const userId = user._id
-    const { cityId } = useParams();
-    const [caption, setCaption] = useState('');
-    const [photo, setPhoto] = useState('');
+  const [posts, setPosts] = useState([]);
+  const authToken = localStorage.getItem("authToken");
+  const headers = {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${authToken}`,
+  };
+  const { user } = useContext(AuthContext);
+  const userId = user._id;
+  const { cityId } = useParams();
+  const [caption, setCaption] = useState("");
+  const [photo, setPhoto] = useState("");
 
+  useEffect(() => {
+    if (cityId) {
+      fetchNewsfeedPosts();
+    }
+  }, [cityId]);
 
-    useEffect(() => {
-        if (cityId) {
-            fetchNewsfeedPosts();
-        }
-    }, [cityId]);
+  const fetchNewsfeedPosts = async () => {
+    try {
+      const response = await axios.get(
+        `${process.env.REACT_APP_SERVER_URL}/posts/${cityId}`,
+        { headers }
+      );
+      setPosts(response.data.posts);
+      console.log(response.data);
+    } catch (error) {
+      console.error("Error fetching newsfeed posts:", error);
+    }
+  };
 
-    const fetchNewsfeedPosts = async () => {
-        try {
-            const response = await axios.get(`${process.env.REACT_APP_SERVER_URL}/posts/${cityId}`, { headers });
-            setPosts(response.data.posts);
-            console.log(response.data); 
-        } catch (error) {
-            console.error('Error fetching newsfeed posts:', error);
-        }
-        
-    };
+  const handleLikeClick = async (postId) => {
+    try {
+      const response = await axios.post(
+        `${process.env.REACT_APP_SERVER_URL}/likes/${postId}`,
+        { userId },
+        { headers }
+      );
+      console.log(response.data);
+      fetchNewsfeedPosts();
+    } catch (error) {
+      console.error(
+        "Error liking post:",
+        error.response?.data || error.message
+      );
+    }
+  };
 
-    const handleLikeClick = async (postId) => {
-        try {
-            const response = await axios.post(
-                `${process.env.REACT_APP_SERVER_URL}/likes/${postId}`,
-                { userId },
-                { headers }
-            );
-            console.log(response.data);
-            fetchNewsfeedPosts();
-        } catch (error) {
-            console.error('Error liking post:', error.response?.data || error.message);
-        }
-    };
+  const handleCreatePost = async () => {
+    try {
+      console.log("Creating post with:", { userId, caption, photo, cityId });
+      const response = await axios.post(
+        `${process.env.REACT_APP_SERVER_URL}/posts/${cityId}`,
+        { user: userId, caption, photo },
+        { headers }
+      );
+      console.log("Post created successfully:", response.data);
+    } catch (error) {
+      console.error(
+        "Error creating post:",
+        error.response?.data || error.message
+      );
+    }
+  };
 
-    const handleCreatePost = async () => {
-        try {
-            console.log("Creating post with:", { userId, caption, photo, cityId });
-          const response = await axios.post(
-            `${process.env.REACT_APP_SERVER_URL}/posts/${cityId}`,
-            { user: userId, caption, photo },
-            { headers }
-          );
-          console.log("Post created successfully:", response.data);
-        } catch (error) {
-          console.error('Error creating post:', error.response?.data || error.message);
-        }
-      };
-
-    return (
+  return (
+    <div>
+      <div>
+        <h1>Create Post</h1>
         <div>
-            <div>
-                <h1>Create Post</h1>
-                <div>
-                    <label>Caption: </label>
-                    <input type="text" value={caption} onChange={(e) => setCaption(e.target.value)} />
-                </div>
-                <div>
-                    <label>Photo URL: </label>
-                    <input type="text" value={photo} onChange={(e) => setPhoto(e.target.value)} />
-                </div>
-                <button onClick={handleCreatePost}>Create Post</button>
-            </div>
-    
-            { posts.map(post => (
-                <div key={post._id}>
-                    <p>{post.caption}</p>
-                    <img src={post.photo} alt={`Post by ${post.user.name}`} />
-                    <div>
-                        <Link to={`/likes/${post._id}`}>{(post.likes && post.likes.length)} likes</Link>
-                        <button onClick={() => handleLikeClick(post._id)}>
-                            {post.likes.includes(userId) ? 'Unlike' : 'Like'}
-                        </button>
-                        <br />
-                        <Link to={`/comments/${post._id}`}>{(post.comments && post.comments.length)} comments</Link>
-                    </div>
-                </div>
-            ))}
+          <label>Caption: </label>
+          <input
+            type="text"
+            value={caption}
+            onChange={(e) => setCaption(e.target.value)}
+          />
         </div>
-    );
+        <div>
+          <label>Photo URL: </label>
+          <input
+            type="text"
+            value={photo}
+            onChange={(e) => setPhoto(e.target.value)}
+          />
+        </div>
+        <button onClick={handleCreatePost}>Create Post</button>
+      </div>
+      <div className="h-screen flex flex-col flex-1 overflow-y-auto">
+        {posts.map((post) => (
+          <div key={post._id}>
+            <p>{post.caption}</p>
+            <img src={post.photo} alt={`Post by ${post.user.name}`} />
+            <div>
+              <Link to={`/likes/${post._id}`}>
+                {post.likes && post.likes.length} likes
+              </Link>
+              <button onClick={() => handleLikeClick(post._id)}>
+                {post.likes.includes(userId) ? "Unlike" : "Like"}
+              </button>
+              <br />
+              <Link to={`/comments/${post._id}`}>
+                {post.comments && post.comments.length} comments
+              </Link>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Newsfeed;
-
-
